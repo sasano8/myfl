8652f16022354a6f6fb5fe5b128ddd2298cb037f
sasano8 <y-sasahara@ys-method.com>
2022-06-05 19:37:24 +0900

    update

--- README.md
+++ README.md
@@ -16,3 +16,27 @@ poetry install
 - https://github.com/allegroai/clearml: 実験管理・モデルサービングなど
 - https://dvc.org/: データバージョン管理・再現可能なパイプライン
 - https://www.mlflow.org/docs/latest/models.html: 実験管理・モデルサービングなど
+
+
+``` python
+import os
+from myfl import WorkSpace, TempWorkSpace
+
+ws = WorkSpace(os.getcwd()).init({})
+```
+
+``` shell
+myfl init
+```
+
+
+``` python
+import os
+from myfl import WorkSpace, TempWorkSpace
+
+print(WorkSpace(os.getcwd()).get_client("default").dataset.list())
+```
+
+``` shell
+myfl dataset list
+```
--- myfl/core/workspaces.py
+++ myfl/core/workspaces.py
@@ -1,26 +1,66 @@
 import tempfile
 import json
 from pathlib import Path
+from typing import Iterable, Union, Any
+import os
+from shutil import rmtree
+
+from pydantic import BaseModel
+from datetime import datetime
 
 
-class WorkSpaceBase:
-    def __str__(self):
-        return str(self.dir)
+class FileInfo(BaseModel):
+    id: str = None
+    name: str
+    path: Path
+    is_file: bool
+    created_at: datetime = None
+    update_at: datetime
+    accessed_at: datetime
+    size: float
 
-    def __truediv__(self, other: str):
-        return self.dir / other  # type: ignore
+    @classmethod
+    def create_from_path(cls, path: Path):
+        # st_mode = 33188  # -node の保護モード
+        # st_ino = 163679  # i-node 番号
+        # st_dev = 2096  # i-nodeが存在するデバイス
+        # st_nlink = 1  # i-nodeへのリンク数
+        # st_uid = 1000  # 所有者のユーザID
+        # st_gid = 1000  # 所有者のグループID
+        # st_size = 870  # バイト単位
+        # st_atime = 1654116369  # 最終アクセス日時
+        # st_mtime = 1653497808  # 最終内容更新日時
+        # st_ctime = 1653497808  # 最終メタデータ更新日時
+        # birthtime           # 一部OSのみ作成日時
 
-    def __enter__(self):
-        return self
+        stat = path.stat()
 
-    def __exit__(self, *args, **kwargs):
-        ...
+        return cls(
+            name=path.name,
+            path=path.absolute(),
+            is_file=path.is_file(),
+            created_at=getattr(stat, "st_birthtime", None),
+            update_at=stat.st_mtime,
+            accessed_at=stat.st_atime,
+            size=stat.st_size,
+        )
 
 
-class WorkSpace(WorkSpaceBase):
-    def __init__(self, dir):
+class WorkSpace:
+    @classmethod
+    def read_model(cls, fd, info: FileInfo):
+        return fd.read()
+
+    @classmethod
+    def write_model(cls, fd, model):
+        fd.write(model)
+
+    def __init__(self, dir: Union[str, Path, tempfile.TemporaryDirectory]):
+        if isinstance(dir, tempfile.TemporaryDirectory):
+            self.temp = dir
+            self.dir = Path(dir.name)
+
         if isinstance(dir, str):
-
             dir = Path(dir)
 
         if not isinstance(dir, Path):
@@ -28,29 +68,136 @@ class WorkSpace(WorkSpaceBase):
 
         self.dir = dir
 
-
-class TempWorkSpace(WorkSpaceBase):
-    def __init__(self, conf: dict = None):
+    def init(self, conf: dict = None):
         if not (isinstance(conf, dict) or conf is None):
             raise TypeError()
 
         conf = conf or {}
+        file_json = self.dir / "conf.json"
+        file_py = self.dir / "conf.py"
 
+        if file_json.exists():
+            raise RuntimeError("Already initialized: `conf.json` already exists.")
+
+        if file_py.exists():
+            raise RuntimeError("Already initialized: `conf.py` already exists.")
+
+        with open(self.dir / "conf.json") as f:
+            json.dump(conf, f, ensure_ascii=False)
+
+    def __str__(self):
+        return str(self.dir)
+
+    def __truediv__(self, other: str) -> Path:
+        return self.dir / other  # type: ignore
+
+    def __enter__(self):
+        if hasattr(self, "temp"):
+            self.temp.__enter__()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        if hasattr(self, "temp"):
+            self.temp.__exit__(*args, **kwargs)
+
+    def get_safe_path(self, path) -> Path:
+        if "/" in path:
+            raise ValueError()
+
+        if ".." in path:
+            raise ValueError()
+
+        return self / path
+
+    def list(self, path, *args, **kwargs) -> Iterable[FileInfo]:
+        p = self.get_safe_path(path)
+        return (
+            FileInfo.create_from_path(x) for x in p.iterdir() if x.name != ".gitignore"
+        )
+
+    def get_info(self, path: str, *args, **kwargs) -> Union[FileInfo, None]:
+        p = self.get_safe_path(path)
+        if not p.exists():
+            return None
+
+        if p.name != ".gitignore":
+            return None
+        return FileInfo.create_from_path(p)
+
+    def get(self, path: str) -> Any:
+        info = self.get_info(path)
+        if not info:
+            raise KeyError("Not Found.")
+
+        with open(info.path, mode="rb") as f:
+            return self.read_model(f, info)
+
+    def put(self, path, model) -> FileInfo:
+        p = self.get_safe_path(path)
+        if p.exists():
+            ...
+        else:
+            ...
+
+        with open(p, mode="wb") as f:
+            self.write_model(f, model)
+            return FileInfo.create_from_path(p)
+
+    def delete(self, path, *args, **kwargs) -> Union[FileInfo, None]:
+        info = self.get_info(path)
+        if not info:
+            return None
+
+        if info.is_file:
+            os.remove(info.path)
+        else:
+            rmtree(info.path, ignore_errors=False)
+
+        return info
+
+
+class TempWorkSpace(WorkSpace):
+    def __init__(self):
         # TODO: 一時ファイルの削除漏れを防ぐには？
         # linux ではスクリプトが終了すると一時ディレクトリは削除される
         # 強制終了などで、削除処理が漏れた場合は、linux では cronによって自動削除される
         # ただし、dockerなどではcronが動かないためゴミが溜まる
-        dir = tempfile.TemporaryDirectory()
-        with open(dir.name + "/conf.json") as f:
-            json.dump(conf, f, ensure_ascii=False)
+        super().__init__(tempfile.TemporaryDirectory())
 
-        self.temp = dir
+    def init(self, conf: dict = None):
+        raise NotImplementedError()
 
-        self.dir = Path(self.temp.name)
+
+class HttpWorkSpace(WorkSpace):
+    def __init__(self, url, *args, **kwargs):
+        self.url = url
+
+    def init(self, conf: dict = None):
+        raise NotImplementedError()
+
+    def __str__(self):
+        return str(self.url)
+
+    def __truediv__(self, other: str):
+        return self.url + "/" + other
 
     def __enter__(self):
-        self.temp.__enter__()
         return self
 
     def __exit__(self, *args, **kwargs):
-        self.temp.__exit__(*args, **kwargs)
+        ...
+
+    def list(self, path, *args, **kwargs):
+        p = self / path
+
+    def get(self, path, *args, **kwargs):
+        p = self / path
+
+    def get_info(self, path, *args, **kwargs):
+        p = self / path
+
+    def put(self, path, *args, **kwargs):
+        p = self / path
+
+    def delete(self, path, *args, **kwargs):
+        p = self / path
